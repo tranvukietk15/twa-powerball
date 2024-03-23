bb4441d4698bc22b675fd10a3af5ae521e6d5233
janos <janos.tran@transwap.com>
2024-03-24 00:38:11 +0700

    change url

--- configure.js
+++ configure.js
@@ -61,7 +61,7 @@ let githubUsername, githubRepo, botUsername;
   ).catch(exitError);
 
   botUsername = getBot.data.result.username;
-  const url = `https://${githubUsername}.github.io/${githubRepo}`;
+  const url = `https://degon.kiettranv.uk/`;
 
   console.log(`\n\nSetting bot ${botUsername} webapp url to ${url}`);
 
